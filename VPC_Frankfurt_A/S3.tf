39f882c4dc2c6b4351c94555a0452d37b0c81adc
Ruslan Variushkin <ruslan.variushkin@tietoevry.com>
2023-05-12 12:47:58 +0200

    feat: add S3 file

---
+++ VPC_Frankfurt_A/S3.tf
@@ -0,0 +1,15 @@
+resource "aws_s3_bucket" "store01" {
+  bucket = "rwx-bucket-store01"
+
+  tags = {
+    Name        = "rwx-backet-store01"
+    Environment = "Dev"
+  }
+}
+
+#resource "aws_s3_bucket_acl" "store1" {
+##  depends_on = [aws_s3_bucket_ownership_controls.example]
+#
+#  bucket = aws_s3_bucket.store01.id
+#  acl    = "private"
+#}
