cd98475719e0f6f83ce0d81ed75e4054b7cd5178
Ruslan Variushkin <ruslan.variushkin@tietoevry.com>
2023-09-15 09:30:50 +0200

    add changes

--- VPC_Frankfurt_A/S3.tf
+++ VPC_Frankfurt_A/S3.tf
@@ -1,11 +1,11 @@
-resource "aws_s3_bucket" "store01" {
-  bucket = "rwx-bucket-store01"
-
-  tags = {
-    Name        = "rwx-backet-store01"
-    Environment = "Dev"
-  }
-}
+##resource "aws_s3_bucket" "store01" {
+##  bucket = "rwx-bucket-store01"
+##
+##  tags = {
+##    Name        = "rwx-backet-store01"
+##    Environment = "Dev"
+##  }
+##}
 
 #resource "aws_s3_bucket_acl" "store1" {
 ##  depends_on = [aws_s3_bucket_ownership_controls.example]
